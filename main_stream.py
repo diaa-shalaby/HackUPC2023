e81504efcaf597f206c8b5a434090f64692811de
Diaa <cadmos149@gmail.com>
2023-05-13 16:13:36 +0200

    renamed main streamlit file

---
+++ main_stream.py
@@ -0,0 +1,10 @@
+import streamlit as st
+from auth0_component import login_button
+
+st.set_page_config(page_title="MLheads",
+                   page_icon="🤯",
+                   layout="wide",
+                   initial_sidebar_state="expanded")
+
+st.title("MLheads 🏠")
+st.markdown("Insert description here")
